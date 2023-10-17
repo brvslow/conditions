b549abb9e986d061de37b8e51a275c5eff6a2ca7
Baptiste Wasson <baptiste.wasson@gmail.com>
2023-10-17 09:15:32 +0200

    Ajout de hello world

--- src/main/java/fr/wasson/App.java
+++ src/main/java/fr/wasson/App.java
@@ -35,6 +35,8 @@ public class App {
             ventesManquantes = QUOTA - nbVente; // Calcul du nombre de ventes manquants pour atteindre le quota de vente minimum pour toucher la prime
 
             // Affichages
+           System.out.println("Hello World !");
+            
             System.out.println("Malheuresement, vous n'avez pas remplie le quota de vente !");
             System.out.println("Votre salaire est de : " + salaireFinal + "€");
             System.out.println("Il vous manque : " + ventesManquantes + " vente(s)");
